ff5f6df295b26ab0ccfbbbbacf68fe50345e4ad2
TC <nothing@nothing.com>
2024-11-23 17:27:03 +1300

    Add main game loop, get and display score

--- code.js
+++ code.js
@@ -6,8 +6,6 @@ function getComputerChoice(){
   return "Scissors";
 }
 
-console.log(getComputerChoice());
-
 function getUserChoice(){
   let userChoice = "";
   while(true) {
@@ -23,26 +21,57 @@ function getUserChoice(){
 
 function playRound(player,computer){
   if (player === computer) {
-    console.log(`Tie with ${player}'s`);
+    console.log(`Tied with ${player}`);
+    return "Tie";
   } else if (
   player === "Rock" && computer === "Scissors" ||
   player === "Scissors" && computer === "Paper" ||
   player === "Paper" && computer === "Rock") {
     console.log(`Player wins! Player:${player} vs Computer:${computer}.`)
+    return "Player"
   } else {
     console.log(`Computer wins.. Player:${player} vs Computer:${computer}.`)
+    return "Computer"
   }
 }
 
+function updateScore(winner){
+  if (winner === "Player") playerScore++;
+  if (winner === "Computer") computerScore++;
+  if (winner === "Tie") ties++;
+}
+
+function displayScore(){
+  console.log(`Score: Player wins: ${playerScore}, Computer Wins ${computerScore}, Ties ${ties}`);
+}
+
+function displayRound(){
+  console.log(`Round: ${round}`);
+}
+
+function displayWinner(){
+  if (playerScore > computerScore) console.log("Player Wins!");
+  if (playerScore < computerScore) console.log("Computer wins..");
+  if (playerScore === computerScore) console.log("tie...")
+}
+
 function game(){
-  const userChoice = getUserChoice();
-  const computerChoice = getComputerChoice();
-  playRound(userChoice,computerChoice);
+  while (round++ != 5) {
+    const userChoice = getUserChoice();
+    const computerChoice = getComputerChoice();
+    displayRound();
+    const winner = playRound(userChoice,computerChoice);
+    updateScore(winner);
+    displayScore();
+  }
+  displayWinner();
+  console.log("Thanks for playing!");
 }
 
 // Game starts here
 let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
+let round = 0;
 
 game();
